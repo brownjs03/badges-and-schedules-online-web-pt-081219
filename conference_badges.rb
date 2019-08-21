07728877dccd135adea3c4ed6dacb05fef2617f6
Jeffrey Brown <brownjs03@gmail.com>
2019-08-21 22:40:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |name|
+    badge_maker(name)
+  #new_array = []
+  #attendees.each do |name|
+    #new_array << badge_maker(name)
+  end 
+  #new_array
+end
+def assign_rooms(attendees)
+  assigned = []
+  counter = 1
+  attendees.each do |name|
+    assigned << "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter += 1 
+  end
+  assigned
+end
+
+def printer(attendees)
+  attendees.each_with_index do |name, index|
+    puts badge_maker(name)
+    puts assign_rooms(name, index)
+end
+end
